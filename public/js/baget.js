9fae7ca091aba796bd7048076f9e6dbc186666b0
Palwan <palwansary1@gmail.com>
2023-11-13 12:42:44 +0500

    frontend step

--- public/js/baget.js
+++ public/js/baget.js
@@ -2,20 +2,37 @@
 const bagetButton = document.getElementById("bagetButton"); // sebet dugmesi
 const baget = document.getElementById("baget"); // sebet konteyniri
 const bagetContainer = document.getElementById("bagetContainer"); // sebet konteyniri
+const bagetSectionEmtyZone = document.getElementById("bagetSectionEmtyZone"); // sebet Section
+const bagetSection = document.getElementById("bagetSection"); // sebet Section
 const addToCartButtons = document.querySelectorAll(".btn-add-to-cart"); // sebede gosmak dugmeleri
 const messagesContainer = document.getElementById('messagesContainer'); // bildirisler konteyneri
 const totalPriceElement = document.getElementById('totalPrice');
 const checkoutForm = document.getElementById('checkoutForm')
 const checkoutButton = document.getElementById('checkoutButton')
 const checkoutWindow = document.getElementById('checkoutWindow')
+const checkoutWindowEmtyZone = document.getElementById('checkoutWindowEmtyZone')
 
 checkoutButton.addEventListener("click", () => { // sebedi acmak yapmak
   checkoutWindow.classList.toggle("active");
+  bagetContainer.classList.toggle("active");
+  bagetSection.classList.toggle("active");
 });
+
+checkoutWindowEmtyZone.addEventListener('click',()=>{
+  checkoutWindow.classList.toggle("active");
+})
 
 bagetButton.addEventListener("click", () => { // sebedi acmak yapmak
   bagetContainer.classList.toggle("active");
+  bagetSection.classList.toggle("active");
 });
+
+bagetSectionEmtyZone.addEventListener('click', ()=>{
+  bagetContainer.classList.toggle("active");
+  bagetSection.classList.toggle("active");
+})
+
+
 
 function updateBaget(cart) { // hazirki sebedi beryar yada tazelap tazesini beryar
   if (cart === null || cart === undefined) {
@@ -33,12 +50,12 @@ function toBagetButtons() {
       let cart = await updateBaget()
       const targetElement = event.target;
       let id = targetElement.dataset.id;
-      const inCart = cart.some((item) => item.id === id);
+      const inCart = cart.some((item) => item.product === id);
       if (inCart) { // eger haryt onden gosulan bolsa
         let successMessageElement = document.createElement('div')
         successMessageElement.setAttribute("id", count);
         successMessageElement.style.animation
-        successMessageElement.innerHTML=`<div id='newMessage_${count}' class='z-20 text-white animate-newMessage p-3 shadow-2xl bg-red-500'>Bu Haryt Öň Hem Sebetde</div>`
+        successMessageElement.innerHTML=`<div id='newMessage_${count}' class='z-20 text-white animate-newMessage p-3 shadow-2xl rounded-md mt-1 bg-red-500'>Bu Haryt Öň Hem Sebetde</div>`
         count++
         messagesContainer.appendChild(successMessageElement)
       } else { // on gosulmadyk bolsa
@@ -51,7 +68,7 @@ function toBagetButtons() {
         });
         let errorMessageElement = document.createElement('div')
         errorMessageElement.setAttribute("id", count);
-        errorMessageElement.innerHTML=`<div id='newMessage_${count}' class='z-20 text-white animate-newMessage p-3 shadow-2xl bg-green-500'>Haryt sebede Goşuldy</div>`
+        errorMessageElement.innerHTML=`<div id='newMessage_${count}' class='z-20 text-white animate-newMessage p-3 shadow-2xl rounded-md mt-1 bg-green-500'>Haryt sebede Goşuldy</div>`
         count++
         messagesContainer.appendChild(errorMessageElement)
         let newCart = await updateBaget(cart);
@@ -68,23 +85,17 @@ function buggetAppend(cart) {
     const itemInCart = document.createElement("div");
     itemInCart.innerHTML = `
             <!-- product box -->
-            <div class="shadow-2xl grid grid-cols-3 w-full h-40 bg-slate-100 rounded-lg relative">
-                <!-- trash box -->
-                <button data-id="${productInCart.product}" class="baget-to-trash  self-center absolute bottom-2 right-2">
-                    <svg  xmlns="http://www.w3.org/2000/svg" class="h-8" viewBox="0 0 512 512">
-                        <path class="stroke-red-500" d="M112 112l20 320c.95 18.49 14.4 32 32 32h184c17.67 0 30.87-13.51 32-32l20-320" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="32"/>
-                        <path class="stroke-red-500" stroke="currentColor" stroke-linecap="round" stroke-miterlimit="10" stroke-width="32" d="M80 112h352"/>
-                        <path class="stroke-red-500" d="M192 112V72h0a23.93 23.93 0 0124-24h80a23.93 23.93 0 0124 24h0v40M256 176v224M184 176l8 224M328 176l-8 224" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="32"/>
-                    </svg>
-                </button>
+            <div class="shadow-2xl grid grid-cols-2 w-full h-24 bg-white rounded-lg overflow-hidden">
+              <!-- col-1 -->
+              <div class="grid grid-cols-2 justify-self-start">       
                 <!-- product image -->
-                <div class="h-40 p-3">
+                <div class="h-24">
                     <img class="h-full w-auto" src="${productInCart.image}" alt="">
                 </div>
 
-                <div class="grid content-evenly -ml-14 my-4">
+                <div class="grid content-evenly">
                     <!-- product name -->
-                    <div class=" text-sm">
+                    <div class="text-sm">
                         ${productInCart.name}
                     </div>
                     <!-- product price -->
@@ -92,12 +103,20 @@ function buggetAppend(cart) {
                         <span class="text-red-500 font-black">${productInCart.price}</span><span class="">TMT</span>
                     </div>
                 </div>
+              </div>
+              <!-- col-2 -->
+              <div class="grid grid-cols-2 gap-x-2 justify-self-end">
                 <!-- quantity -->
-                <div class="justify-self-end self-center mr-3 flex space-x-1">
+                <div class="self-center grid">
                     <span data-id="${productInCart.product}" class="quantity-increase-button select-none cursor-pointer bg-slate-300 h-8 w-8 rounded-full grid place-items-center font-bold">+</span>
-                    <input type="text" value="${productInCart.quantity}" id="quantity-input-${productInCart.product}" class="w-8 text-center focus:outline-none focus:ring rounded-sm">
+                    <input data-id="${productInCart.product}" type="number" value="${productInCart.quantity}" min="1" id="quantity-input-${productInCart.product}" class="quantity-input w-8 text-center focus:outline-none focus:ring rounded-sm">
                     <span data-id="${productInCart.product}" class="quantity-reduction-button select-none cursor-pointer bg-slate-300 h-8 w-8 rounded-full grid place-items-center font-bold">-</span>
-                </div>        
+                </div>    
+                <!-- trash box -->
+                <button data-id="${productInCart.product}" class="baget-to-trash w-5 bg-gary-500">
+                <img  src="/trash.svg" class="w-full" alt="">
+                </button>  
+              </div>  
             </div>
         `;
     baget.appendChild(itemInCart); // sebede haryt gosyar
@@ -140,6 +159,20 @@ function buggetAppend(cart) {
       inCartTotal()
     })
   })
+
+  const quantityInput = document.querySelectorAll('.quantity-input')
+  quantityInput.forEach(input=>{
+    input.addEventListener('input', (event)=>{
+      const id = event.target.dataset.id
+      let value =  event.target.value
+      value < 1 ? value=1 : value=value
+      const product = cart.find((item) => item.product === id);
+      product.quantity = value;
+      updateBaget(cart)
+      inCartTotal()
+    })
+  })
+
 }
 
 function inCartTotal() {
@@ -147,7 +180,7 @@ function inCartTotal() {
   const totalPrice = cart.reduce((acc, item) => {
     return acc + item.price * item.quantity;
   }, 0);
-  totalPriceElement.innerHTML = 'Jemi Baha: ' + totalPrice;
+  totalPriceElement.innerText = 'Jemi Baha: ' + totalPrice;
 }
 
 checkoutForm.addEventListener('submit', async e=>{
@@ -178,7 +211,6 @@ checkoutForm.addEventListener('submit', async e=>{
       headers: { "Content-Type": "application/json" }
     }).then(response=> response.json())
       .then(data=>{
-        console.log(data);
         location.assign("http://localhost:3050/api/v1/orders/"+data.insertId)
       })
   } catch (error) {
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./views/**/*.{html,js,ejs}", "./public/**/*.{html,js,ejs}"],
+  content: ["./views/**/*.{html,js,ejs}", "./public/**/*.{html,js,ejs,svg}"],
   theme: {
     extend: {
       keyframes: {
